8a6cb5dcd65bebf88d0ad7e06d8d808c53d46677
Shivam Pawar <shivamp1019@gmail.com>
2024-11-13 14:43:13 +0530

    Final Commit

---
+++ ingest.py
@@ -0,0 +1,29 @@
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter 
+
+DATA_PATH = 'data/'
+DB_FAISS_PATH = 'vectorstore/'
+
+# Create vector database
+def create_vector_db():
+    loader = DirectoryLoader(DATA_PATH,
+                             glob='*.pdf',
+                             loader_cls=PyPDFLoader)
+
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=200,
+                                                   chunk_overlap=25)
+    texts = text_splitter.split_documents(documents)
+
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
+                                       model_kwargs={'device': 'cpu'},
+                                       allow_dangerous_deserialization=True)
+
+    db = FAISS.from_documents(texts, embeddings)
+    db.save_local(DB_FAISS_PATH)
+
+if __name__ == "__main__":
+    create_vector_db()
+
